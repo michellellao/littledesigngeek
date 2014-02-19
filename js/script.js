100d3991591bb8368b426b427eb4ecc68229b482
Michelle Lao <michellellao.ai@gmail.com>
2014-02-19 08:50:03 -0800

    added slides

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 	$('.third-section').css('min-height', $(window).height()/3);
 	$('#email').click(function() {
 		$('#myemail').fadeToggle('200');
-	})
+	});
+	$('.slide-title-large').css('right', $('.rslides').width()/40);
+	$('.slide-title-large').css('bottom', $('.rslides').height()/50);
 });
 
