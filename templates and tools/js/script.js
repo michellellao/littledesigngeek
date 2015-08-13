cd21afc30c82c0235b4cdc3015d872350b7be444
Michelle Lao <michellellao.ai@gmail.com>
2015-08-13 11:03:23 -0700

    script change

--- templates and tools/js/script.js
+++ templates and tools/js/script.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-	$("#raawr").click(function(){
-  		$("#raawr-expand").addClass("clearfix sm-12 md-12 lg-12");
-  		$("#raawr-expand").html("<p>description!!</p>")
-  		$("#raawr-expand").css("display" , "block")
-	});
+	
 });
 
