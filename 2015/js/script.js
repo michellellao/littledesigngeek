d0680beb0855ad42f7e65c1f0e27a758f9897dd1
michellellao <michellellao.ai@gmail.com>
2018-03-08 09:01:39 -0800

    Add files via upload

---
+++ 2015/js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+  $('a[href^="#"]').on('click',function (e) {
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
+});
+
+// Responsive Slides
+$(document).ready(function() {
+  $('#email').click(function() {
+    $('#myemail').fadeIn('200');
+  });
+  $('.slide-title-large').css('right', $('.rslides').width()/40);
+  $('.slide-title-large').css('bottom', $('.rslides').height()/50);
+});
+
+// Bar Animation
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.bar .fill');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+    checkAnimation();
+});
